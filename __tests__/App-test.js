ffcfec41cea5db4fe80cf7dff4076ef802f371f5
sourceship <artificalpeter@gmail.com>
2021-06-30 17:27:04 -0500

    started adding more tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,19 @@
-/**
- * @format
- */
 
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import Chart from '../src/home/Chart'
+import {create} from 'react-test-renderer'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { Dashboard } from '../src/home/Dashboard';
 
-it('renders correctly', () => {
+test('renders correctly', () => {
   renderer.create(<App />);
+});
+
+const chartTree = create(<Dashboard/>)
+test('chart renders correctly', () => {
+  expect(chartTree).toMatchSnapshot()
 });
